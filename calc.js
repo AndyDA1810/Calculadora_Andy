c9b9fc4a7a8a9d6d562db83cb65c22d4a1d0fb48
AndyDA1810 <andreadoaf@gmail.com>
2023-03-29 00:43:29 -0500

    se agrego el template

--- calc.js
+++ calc.js
@@ -2,26 +2,26 @@ const sumar=()=> {
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
     var resultado = num1 + num2;
-    document.getElementById("resultado").value = resultado;
+    document.getElementById("resultado").value = `El resultado es: ${ resultado}`;
 }
 
 const restar =()=> { 
     var num1 = parseInt(document.getElementById("num1").value); 
     var num2 = parseInt(document.getElementById("num2").value); 
     var resultado = num1 - num2; 
-    document.getElementById("resultado").value = resultado; 
+    document.getElementById("resultado").value = `El resultado es: ${ resultado}`; 
 }
 
 const multiplicar=()=> {
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
     var resultado = num1 * num2;
-    document.getElementById("resultado").value = resultado;
+    document.getElementById("resultado").value = `El resultado es: ${ resultado}`;
 }
 
 const dividir=()=> {
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
     var resultado = num1 / num2;
-    document.getElementById("resultado").value = resultado;
+    document.getElementById("resultado").value = `El resultado es: ${ resultado}`;
 }
