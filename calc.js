933db48431a6bb2b8eaebe033794a4a86ddea763
AndyDA1810 <andreadoaf@gmail.com>
2023-03-29 00:33:46 -0500

    cambios de funcion a funcion flecha

--- calc.js
+++ calc.js
@@ -1,25 +1,25 @@
-function sumar() {
+const sumar=()=> {
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
     var resultado = num1 + num2;
     document.getElementById("resultado").value = resultado;
 }
 
-function restar() {
-    var num1 = parseInt(document.getElementById("num1").value);
-    var num2 = parseInt(document.getElementById("num2").value);
-    var resultado = num1 - num2;
-    document.getElementById("resultado").value = resultado;
+const restar =()=> { 
+    var num1 = parseInt(document.getElementById("num1").value); 
+    var num2 = parseInt(document.getElementById("num2").value); 
+    var resultado = num1 - num2; 
+    document.getElementById("resultado").value = resultado; 
 }
 
-function multiplicar() {
+const multiplicar=()=> {
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
     var resultado = num1 * num2;
     document.getElementById("resultado").value = resultado;
 }
 
-function dividir() {
+const dividir=()=> {
     var num1 = parseInt(document.getElementById("num1").value);
     var num2 = parseInt(document.getElementById("num2").value);
     var resultado = num1 / num2;
